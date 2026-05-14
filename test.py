2ef127137d202533f40386864380b7ab2e29ddb8
Spandana365 <spandanamraikar83@gmail.com>
2026-05-14 19:53:12 +0530

    update2

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-import sys
-import platform
 
-def test_deployment():
-    print("--- Deployment Test ---")
-    print(f"Python Version: {sys.version}")
-    print(f"Operating System: {platform.system()} {platform.release()}")
-    print("Status: Success! The script is running on the server.")
-
-if __name__ == "__main__":
-    test_deployment()
